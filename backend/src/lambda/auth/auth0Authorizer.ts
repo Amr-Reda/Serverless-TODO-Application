9760c974568acdc745275137d1874ab2656f45f2
Amr-Reda <amrreda547@gmail.com>
2020-03-31 21:18:02 +0200

    implement verify auth function

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,16 +3,17 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// const jwksUrl = 'https://dev-gbcxzine.auth0.com/.well-known/jwks.json'
+
+const auth0Secret = process.env.AUTH_O_SECRET
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +59,9 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return verify(token, auth0Secret, {algorithms: ['HS256']}) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
