5c8c0c5dbdb3a125641265e0126df48c6edfa413
Amr-Reda <amrreda547@gmail.com>
2020-04-03 01:26:24 +0200

    implement upload image to s3 function

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,30 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const bucketName = process.env.IMAGES_S3_BUCKET
+const s3 = new AWS.S3()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   console.log(todoId);
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: 300
+  })
   
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  return {
+    statusCode: 201,
+       headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(
+      {
+        uploadUrl
+      }
+    )
+  }
 }
